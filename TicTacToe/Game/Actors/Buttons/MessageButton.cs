56107128344d151e9e71f4a164e200b896537866
Amakazor <amakazor@gmail.com>
2020-09-26 15:16:21 +0200

    Implemented MessageButton

---
+++ TicTacToe/Game/Actors/Buttons/MessageButton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Window;
+using TicTacToe.Game.Data;
+using TicTacToe.Utility;
+
+namespace TicTacToe.Game.Actors.Buttons
+{
+    class MessageButton : Button
+    {
+        public MessageType MessageType { get; }
+
+        public MessageButton(Position position, Position relativeTextPosition, Gamestate gamestate, string text, MessageType messageType) : base(position, relativeTextPosition, gamestate, text)
+        {
+            MessageType = messageType;
+        }
+
+        public override void OnClick(MouseButtonEventArgs args)
+        {
+            MessageBus.Instance.PostEvent(MessageType, this, args);
+        }
+    }
+}
