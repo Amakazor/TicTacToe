9fd439c2a3f4aa1a20b3a7696bbc0af4d5f52071
Amakazor <amakazor@gmail.com>
2020-09-30 22:25:21 +0200

    Implemented SelectSymbolButton

---
+++ TicTacToe/Game/Actors/Buttons/SelectSymbolButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Window;
+using TicTacToe.Game.Data;
+using TicTacToe.Utility;
+
+namespace TicTacToe.Game.Actors.Buttons
+{
+    class SelectSymbolButton : Button
+    {
+        public Symbol Symbol { get; private set; }
+        public SelectSymbolButton(Position position, Position relativeTextPosition, Gamestate gamestate, string text, Symbol symbol) : base(position, relativeTextPosition, gamestate, text)
+        {
+            Symbol = symbol;
+        }
+
+        public override void OnClick(MouseButtonEventArgs args)
+        {
+            Gamestate.NewPlayer.SymbolData.texture = Symbol.Texture;
+        }
+    }
+}
