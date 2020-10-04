b8bb7110d02cd6de11be5afc7413abe4926ba920
Amakazor <amakazor@gmail.com>
2020-10-04 22:02:13 +0200

    Statistics part 1

---
+++ TicTacToe/Game/Data/Statistic.cs
@@ -0,0 +1,20 @@
+﻿namespace TicTacToe.Game.Data
+{
+    public struct Statistic
+    {
+        int Player;
+        int Total;
+        int Won;
+        int Lost;
+        int Draws;
+
+        public Statistic(int player, int total, int won, int lost, int draws)
+        {
+            Player = player;
+            Total = total;
+            Won = won;
+            Lost = lost;
+            Draws = draws;
+        }
+    }
+}
---
+++ TicTacToe/Game/Data/StatisticsManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace TicTacToe.Game.Data
+{
+    class StatisticsManager
+    {
+        public Statistic LoadPlayersStatistics(int playerID)
+        {
+            XDocument textureConfig = XDocument.Load("assets/data/Statistics.xml");
+        }
+    }
+}
