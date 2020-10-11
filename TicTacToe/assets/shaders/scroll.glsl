ca9e0afc63907bb03ea15a40d968c83214e17094
Amakazor <amakazor@gmail.com>
2020-10-11 15:19:27 +0200

    Tweaked background shader

--- TicTacToe/assets/shaders/scroll.glsl
+++ TicTacToe/assets/shaders/scroll.glsl
@@ -6,6 +6,6 @@ void main()
 {
     vec2 texCoord;
     texCoord.x = fract((gl_FragCoord.x / resolution.x) + time * 0.25);
-    texCoord.y = fract((gl_FragCoord.y / resolution.y) + time * 0.5);
+    texCoord.y = fract((gl_FragCoord.y / resolution.y) + time * 0.1);
     gl_FragColor = texture2D(texture, texCoord);
 }
