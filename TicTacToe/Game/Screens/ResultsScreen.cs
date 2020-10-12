5a4fcaf050f1ee52a61a99f4ae79cb49e22d279e
amakazor <amakazor@gmail.com>
2020-10-12 22:55:57 +0200

    Results screen refactor

--- TicTacToe/Game/Screens/ResultsScreen.cs
+++ TicTacToe/Game/Screens/ResultsScreen.cs
@@ -14,18 +14,20 @@ namespace TicTacToe.Game.Screens
     internal class ResultsScreen : Screen
     {
         private TextBox TextBox;
-        private List<TextButton> Buttons;
+        private List<Button> Buttons;
 
         public ResultsScreen(Gamestate gamestate) : base(gamestate, ScreenType.Results)
         {
             SaveStatistics();
 
-            TextBox = new TextBox(new Position(200, 0, 600, 100), Gamestate, new Vector2f(), 40, TextPosition.Middle, TextPosition.Middle, GetResultsText());
+            TextBox = new TextBox(new Position(200, 25, 600, 100), Gamestate, new Vector2f(), 50, TextPosition.Middle, TextPosition.Middle, GetResultsText());
 
-            Buttons = new List<TextButton>();
-            Buttons.Add(new ActionButton(new Position(200, 220, 600, 100), Gamestate, new Vector2f(), 40, TextPosition.Middle, TextPosition.Middle, "Rematch", (MouseButtonEventArgs args) => { Gamestate.ChangePlayer(); MessageBus.Instance.PostEvent(MessageType.ChangeScreen, this, new ChangeScreenEventArgs { Screen = ScreenType.Game }); }));
-            Buttons.Add(new ActionButton(new Position(200, 440, 600, 100), Gamestate, new Vector2f(), 40, TextPosition.Middle, TextPosition.Middle, "New Game", (MouseButtonEventArgs args) => { Gamestate.Clear(); MessageBus.Instance.PostEvent(MessageType.ChangeScreen, this, new ChangeScreenEventArgs { Screen = ScreenType.Pregame }); }));
-            Buttons.Add(new ScreenChangeButton(new Position(200, 640, 600, 100), Gamestate, new Vector2f(), 40, TextPosition.Middle, TextPosition.Middle, "Return to Main Menu", ScreenType.MenuScreen));
+            Buttons = new List<Button>();
+            Buttons.Add(new ActionButton(new Position(200, 150, 600, 100), Gamestate, new Vector2f(), 40, TextPosition.Middle, TextPosition.Middle, "Rematch", (MouseButtonEventArgs args) => { Gamestate.ChangePlayer(); MessageBus.Instance.PostEvent(MessageType.ChangeScreen, this, new ChangeScreenEventArgs { Screen = ScreenType.Game }); }));
+            Buttons.Add(new ActionButton(new Position(200, 300, 600, 100), Gamestate, new Vector2f(), 40, TextPosition.Middle, TextPosition.Middle, "New Game", (MouseButtonEventArgs args) => { Gamestate.Clear(); MessageBus.Instance.PostEvent(MessageType.ChangeScreen, this, new ChangeScreenEventArgs { Screen = ScreenType.Pregame }); }));
+
+            Buttons.Add(new ReturnButton(new Position(25, 875, 100, 100), Gamestate, Gamestate.TextureAtlas.TexturesDictionary[TextureType.Icon]["back"], ScreenType.MenuScreen));
+
         }
 
         public override void Dispose() { }
@@ -37,7 +39,7 @@ namespace TicTacToe.Game.Screens
             TextBox.RecalculateComponentsPositions();
             renderObjects.AddRange(TextBox.GetRenderObjects());
 
-            foreach (TextButton button in Buttons)
+            foreach (Button button in Buttons)
             {
                 button.RecalculateComponentsPositions();
                 renderObjects.AddRange(button.GetRenderObjects());
